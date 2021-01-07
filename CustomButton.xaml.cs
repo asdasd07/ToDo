770d05a56c54d678e28c7397d1203416c17bd05b
CLEVO\Artur <dziubich@interia.eu>
2021-01-07 13:21:16 +0100

    Version 1.0

--- CustomButton.xaml.cs
+++ CustomButton.xaml.cs
@@ -1,27 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Controls;
-using System.Windows;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ToDo {
-    class CustomButton : UserControl2 {
+    /// <summary>
+    /// Logika interakcji dla klasy CustomButton.xaml
+    /// </summary>
+    public partial class CustomButton : UserControl {
         public int id, groupID;
-        public string title="", description="", group="";
+        public string title = "", description = "", group = "";
         public DateTime creation, end, deadline;
         public bool complete;
         public CustomButton() {
+            InitializeComponent();
         }
         public CustomButton(System.Data.SQLite.SQLiteDataReader data) {
+            InitializeComponent();
             id = int.Parse(data["id"].ToString());
             groupID = int.Parse(data["groupid"].ToString());
             title = data["title"].ToString();
@@ -39,9 +33,8 @@ namespace ToDo {
             L4.Content = group;
             tex.Text = description;
             if (complete) {
-                Background= new SolidColorBrush(Colors.Gray) { Opacity = 0.5 };
+                Background = new SolidColorBrush(Colors.Gray) { Opacity = 0.5 };
             }
         }
-
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #ToDo
 Simple program using .NET Framework WPF and local SQLite database.
 It allows you to plan things to do with deadlines for individual groups of employees.
+
+![Screenshot](Readme-ToDo.png)
